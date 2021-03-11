f2f2c0147a29b48c0f4da00ff7ef46f3d102b5fc
Barry Quinn <b.quinn@qub.ac.uk>
2021-03-11 10:56:34 +0000

    from macbook pro

--- R/ati-package.R
+++ R/ati-package.R
@@ -1,6 +1,6 @@
 #' @keywords internal
 "_PACKAGE"
-#' @import  ggplot2
+
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
