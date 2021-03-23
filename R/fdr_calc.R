927227dc7a1f021d3b2e089e8d2ff8f584f5f138
Barry Quinn <b.quinn@qub.ac.uk>
2021-03-23 15:37:25 +0000

    new function fdr_calc

--- R/fdr_calc.R
+++ R/fdr_calc.R
@@ -1,3 +1,19 @@
+#' False discovery rate
+#'
+#' The function express the false discovery rate associated with selection bias in multiple testing.
+#' This produces a tibble of the recall, precision and false discovery rate
+#' when give the true probability of a positive results, the significance level,
+#' ,the false negative rate (Type II error) and the number of trails.
+#' @param ground_truth double probability of a positive results
+#' @param alpha double level of significance
+#' @param beta double Type || error
+#' @param trails integer No of trails
+#'
+#' @return tibble
+#' @export
+#'
+#' @examples
+#' fdr(0.01,beta=0.4,trails=10000)
 fdr_calc <- function(ground_truth,alpha=0.05,beta,trails) {
   theta=ground_truth/(1-ground_truth)
   recall=1-beta
